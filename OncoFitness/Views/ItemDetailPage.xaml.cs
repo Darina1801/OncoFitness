0f2c30ad343c4190d2ec8912bb235a1850b1f252
Darina Kavokina <kuz-dana@mail.ru>
2020-12-31 22:27:06 +0100

    Добавьте файлы проекта.

---
+++ OncoFitness/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using OncoFitness.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace OncoFitness.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
