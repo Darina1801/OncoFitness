e0c2b1788590d5df25c247cf6be6923c4e722ac7
Darina Kavokina <kuz-dana@mail.ru>
2021-01-11 21:40:35 +0100

    Added StartTraining page and its markup. Added mock data to StartTrainingViewModel. Added Exercise model.

---
+++ OncoFitness/Models/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OncoFitness.Models
+{
+	public class Exercise
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string ImagePath { get; set; }
+		public int RepeatsCount { get; set; }
+		public string Description { get; set; }
+	}
+}
