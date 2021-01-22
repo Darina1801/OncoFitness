4a924d6aca0d9cfcbc2f0f4349d7e6deb5783124
Darina Kavokina <kuz-dana@mail.ru>
2021-01-22 11:11:22 +0100

    Changed AppShell to display New Plan itstead of Notifications. Added NewPlanPage. Notifications moved to the SettingsPage.

---
+++ OncoFitness/ViewModels/NewPalnViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OncoFitness.ViewModels
+{
+	public class NewPlanViewModel : BaseViewModel
+	{
+		public NewPlanViewModel()
+		{
+			Title = "Свой план";
+
+			//Items = new ObservableCollection<Training>();
+			//LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+			//StartTrainingCommand = new Command(async () => await ExecuteStartTrainingCommand());
+
+			//ItemTapped = new Command<Training>(OnItemSelected);
+
+			//MyListView.ItemsSource = Items;
+		}
+	}
+}
---
+++ OncoFitness/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OncoFitness.ViewModels
+{
+	public class SettingsViewModel : BaseViewModel
+	{
+		public SettingsViewModel()
+		{
+			Title = "Настройки";
+		}
+	}
+}
