3fff6b0061bdaffa80fcc922308d74912f7ac8e1
Darina Kavokina <kuz-dana@mail.ru>
2021-03-11 21:31:52 +0100

    Updated TrrgetPlatform from Andriod 9.0 to Android 10.0. Made two buttons instead of one. Implemented buttons switch once Exercise has started/finished.

--- OncoFitness/ViewModels/StartTrainingViewModel.cs
+++ OncoFitness/ViewModels/StartTrainingViewModel.cs
@@ -14,10 +14,42 @@ namespace OncoFitness.ViewModels
 	{
 		public ObservableCollection<Exercise> Items { get; }
 		public Command FinishTrainingCommand { get; }
+		public Command StartNewExerciseCommand { get; }
+		public Command PauseExerciseCommand { get; }
+		public Command StopExerciseCommand { get; }
+		public Exercise CurrentExercise { get; set; }
+		public bool PlayFinishTrainingButtonsVisibility 
+		{
+			get { return playFinishTrainingButtonsVisibility; }
+			set 
+			{ 
+				playFinishTrainingButtonsVisibility = value;
+				OnPropertyChanged(); 
+			} 
+		}
+		public bool PauseStopButtonsVisibility
+		{
+			get { return pauseStopButtonsVisibility; }
+			set
+			{
+				pauseStopButtonsVisibility = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private int currentExerciseNumber = 0;
+		private bool playFinishTrainingButtonsVisibility;
+		private bool pauseStopButtonsVisibility;
 
 		public StartTrainingViewModel()
 		{
 			FinishTrainingCommand = new Command(async () => await ExecuteFinishTrainingCommand());
+			StartNewExerciseCommand = new Command(async () => await ExecuteStartNewExerciseCommand());
+			PauseExerciseCommand = new Command(async () => await ExecutePauseExerciseCommand());
+			StopExerciseCommand = new Command(async () => await ExecuteStopExerciseCommand()); 
+
+			PlayFinishTrainingButtonsVisibility = true;
+			PauseStopButtonsVisibility = false;
 
 			Items = new ObservableCollection<Exercise>()
 			{ 
@@ -36,6 +68,74 @@ namespace OncoFitness.ViewModels
 			try
 			{
 				await Shell.Current.GoToAsync(nameof(EndTrainingPage));
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+
+		async Task ExecuteStartNewExerciseCommand()
+		{
+			IsBusy = true;
+
+			try
+			{
+				if (currentExerciseNumber == Items.Count)
+				{
+					IsBusy = false;
+					return;
+				}
+				CurrentExercise = Items[currentExerciseNumber];
+				currentExerciseNumber++;
+				PlayFinishTrainingButtonsVisibility = false;
+				PauseStopButtonsVisibility = true;
+				//Implement StartTimer call
+				//await Shell.Current.GoToAsync(nameof(EndTrainingPage));
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+
+		async Task ExecutePauseExerciseCommand()
+		{
+			IsBusy = true;
+
+			try
+			{
+				//Implement PauseTimer call
+				//await Shell.Current.GoToAsync(nameof(EndTrainingPage));
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+
+		async Task ExecuteStopExerciseCommand()
+		{
+			IsBusy = true;
+
+			try
+			{
+				PlayFinishTrainingButtonsVisibility = true;
+				PauseStopButtonsVisibility = false;
+				//Implement StopTimer call
+				//await Shell.Current.GoToAsync(nameof(EndTrainingPage));
 			}
 			catch (Exception ex)
 			{
